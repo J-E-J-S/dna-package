0ae0bc3c50cd270e49ad011379e09c6506138d65
J-E-J-S <james.sanders1711@gmail.com>
2021-02-03 21:08:33 +0000

    DNA reverse complement fn

--- dna/dna.py
+++ dna/dna.py
@@ -17,4 +17,22 @@ class Dna:
 
         return comp
 
-    
+    def transcribe(self):
+
+        return self.sequence.replace('T', 'U')
+
+    def reverse_complement(self):
+
+        paired_sequence = ""
+
+        for base in self.sequence:
+            if base == "A":
+                paired_sequence += "T"
+            if base == "T":
+                paired_sequence += "A"
+            if base == "G":
+                paired_sequence += "C"
+            if base == "C":
+                paired_sequence += "G"
+
+        return paired_sequence[::1]
